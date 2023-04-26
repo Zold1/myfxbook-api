0990f8cd4e530d3d4a3dfb6042ed338c35a77e40
isaiah <isaiah98981@icloud.com>
2023-04-26 16:31:11 -0700

    pip install myfxbook

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ I made a simple class to make it easier to use Myfxbook
 To get to know its functions and parameters, you can find out from the official website [Myfxbook API](https://www.myfxbook.com/api)
 
 ## How to use
+```python
+pip install myfxbook
+```
 
 ``` python
 import myfxbook
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='myfxbook',
+    version='0.1',
+    description='Python wrapper for the Myfxbook API',
+    author='isaihbjork',
+    author_email='labsmodev@gmail.com',
+    url='https://github.com/isaiahbjork/myfxbook',
+    packages=['myfxbook'],
+    install_requires=['requests'],
+)
